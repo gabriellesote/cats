fb484520e6d58853a73f18a71ee41ba01004e586
gabriellesote <gabrielle.sote@gmail.com>
2024-11-24 16:18:04 -0300

    rotas de de pegar por id, deletar, dar update, criar estão prontas

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -3,17 +3,19 @@ const prisma = new PrismaClient();
 
 export const createPic = async (req, res) => {
   try {
-      // Certifique-se de enviar 'name' no corpo da requisição
+    // Certifique-se de enviar 'name' no corpo da requisição
     const file = req.file;
     const name = req.file.filename;
 
     if (!name || !file) {
-      return res.status(400).json({ error: "O campo 'name' e o arquivo são obrigatórios." });
+      return res
+        .status(400)
+        .json({ error: "O campo 'name' e o arquivo são obrigatórios." });
     }
 
     const newPic = await prisma.midia.create({
       data: {
-        name : name,
+        name: name,
         src: file.path.replace(/\\/g, "/"), // Corrige o caminho para o formato correto
         type: req.file.mimetype,
         description: req.body.description || "", // Descrição opcional
@@ -27,12 +29,72 @@ export const createPic = async (req, res) => {
 };
 
 export const getPics = async (req, res) => {
-  try{
+  try {
     const pic = await prisma.midia.findMany();
     res.status(200).json(pic);
-
-
-  }catch(error){
-    res.status(400).json({error:error.message})
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-}
+};
+
+export const getPicsById = async (req, res) => {
+  try {
+    const pic = await prisma.midia.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!pic) {
+      return res.status(404).json({ message: "Foto não encontrado!" });
+    }
+
+    res.status(200).json(pic);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const updatePic = async (req, res) => {
+  try {
+    const file = req.file;
+    const name = req.file.filename;
+
+    if (!name || !file) {
+      return res
+        .status(400)
+        .json({ error: "O campo 'name' e o arquivo são obrigatórios." });
+    }
+
+
+    const pic = await prisma.midia.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        name: name,
+        src: file.path.replace(/\\/g, "/"), // Corrige o caminho para o formato correto
+        type: req.file.mimetype,
+        description: req.body.description || "",
+      },
+    });
+    res.status(200).json({ pic, msg: "Imagem atualizada com sucesso!" });
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const deletePic = async (req, res) => {
+  try {
+    await prisma.midia.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({ message: "Foto deletada com sucesso!" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
--- routes/pictureRoutes.js
+++ routes/pictureRoutes.js
@@ -1,19 +1,26 @@
 import express from "express";
-import { createPic,getPics } from "../controllers/pictureController.js";
-import  upload from '../config/multer.js'
- 
+import {
+  createPic,
+  getPics,
+  getPicsById,
+  updatePic,
+  deletePic
+} from "../controllers/pictureController.js";
+import upload from "../config/multer.js";
 
 const router = express.Router();
 
-router.post("/upload", upload.single("file"),createPic, (req, res) => {
+router.post("/upload", upload.single("file"), createPic, (req, res) => {
   if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
+    return res.status(400).json({ error: "No file uploaded" });
   }
-  res.json({ message: 'File uploaded successfully', file: req.file });
+  res.json({ message: "File uploaded successfully", file: req.file });
 });
 
 router.get("", getPics);
+router.get("/:id", getPicsById)
+router.put("/update/:id", upload.single("file"), updatePic);
+router.delete("/:id", deletePic);
+ 
 
 export default router;
-
- 
