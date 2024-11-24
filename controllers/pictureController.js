f37100a824010533d61c36f1c0e76729188bfa1f
gabriellesote <gabrielle.sote@gmail.com>
2024-11-24 19:32:13 -0300

    adicionando assets para o readme

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 export const createPic = async (req, res) => {
   try {
     // Certifique-se de enviar 'name' no corpo da requisição
-    const file = req.file;
+    const file = req.files  || [];
     const name = req.file.filename;
 
     if (!name || !file) {
@@ -13,16 +13,26 @@ export const createPic = async (req, res) => {
         .json({ error: "O campo 'name' e o arquivo são obrigatórios." });
     }
 
-    const newPic = await prisma.midia.create({
-      data: {
-        name: name,
-        src: file.path.replace(/\\/g, "/"), // Corrige o caminho para o formato correto
-        type: req.file.mimetype,
-        description: req.body.description || "", // Descrição opcional
-      },
-    });
+ 
 
-    res.status(201).json({ pic: newPic, msg: "Imagem salva com sucesso!" });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- routes/pictureRoutes.js
+++ routes/pictureRoutes.js
@@ -11,31 +11,17 @@ import { generateToken, validateToken} from "../config/token_gen.js";
 
 const router = express.Router();
 
-// Rota para upload de imagens - proteção com token
-router.post("/upload", validateToken, upload.single("file"), createPic, (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
+router.post("/upload", validateToken, upload.array("files", 10), createPic, (req, res) => {
+  if (!req.files || req.files.length === 0) {
+    
+    return res.status(400).json({ error: "No files uploaded" });
   }
-  res.json({ message: "File uploaded successfully", file: req.file });
+  res.json({ message: "Files uploaded successfully", files: req.files });
 });
-
-// Rota para listar todas as imagens - proteção com token
 router.get("", validateToken, getPics);
-
-// Rota para buscar imagem por id - proteção com token
 router.get("/:id", validateToken, getPicsById);
-
-// Rota para atualizar imagem - proteção com token
 router.put("/update/:id", validateToken, upload.single("file"), updatePic);
-
-// Rota para deletar imagem - proteção com token
 router.delete("/:id", validateToken, deletePic);
-
-// Rota para gerar token
 router.post('/genToken', generateToken);
-
-
-// Rota para listar imagens - se desejado, pode ser pública, sem validação de token
-// router.post('/list', validateToken); // caso você queira também proteger essa rota com token
 
 export default router;
